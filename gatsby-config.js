ae14e5c2ff6f5ce8a6c4c83ebe8150e57df5ec80
Andres Espejo <jeaespejogo@unal.edu.co>
2020-11-16 11:44:02 -0500

    Configuration initial for gatsby.config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Travel App`,
+    description: `This is a website that contains the best travel destination for you.`,
+    author: `Jefferson Andrés Espejo Goez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
